f4d5ead391961649d13f12c124143079d694498f
Daniel Šulik <sulikdan@fit.cvut.cz>
2020-10-23 21:48:38 +0200

    Adding swagger(partially), there will be more later.

--- src/main/java/com/sulikdan/ocrApi/entities/DocumentProcessStatus.java
+++ src/main/java/com/sulikdan/ocrApi/entities/DocumentProcessStatus.java
@@ -3,7 +3,7 @@ package com.sulikdan.ocrApi.entities;
 /**
  * Created by Daniel Šulik on 08-Jul-20
  *
- * <p>Class DocumentStatus is used for .....
+ * <p>Class DocumentStatus class representing current status of received document.
  */
 public enum DocumentProcessStatus {
   PROCESSING,
