395272709f851dc887d7729a48527d7ff19c8f58
Daniel Sulik <sulikdan@outlook.com>
2024-03-23 21:11:12 +0100

    Basic footer + changelog part

---
+++ frontend/ocr-simple-frontend/src/app/body/about/changes-list/changes-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-changes-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './changes-list.component.html',
+  styleUrl: './changes-list.component.sass'
+})
+export class ChangesListComponent {
+
+}
