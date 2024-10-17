52dd4ca4c64caa09080c4f52837801647e6c9a3e
Daniel Sulik <sulikdan@outlook.com>
2024-10-17 22:27:03 +0200

    Using ProblemDetail

--- backend/src/main/java/com/sulikdan/ocrApi/controllers/ErrorHandlers/DocumentUploadExceptionAdvice.java
+++ backend/src/main/java/com/sulikdan/ocrApi/controllers/ErrorHandlers/DocumentUploadExceptionAdvice.java
@@ -5,7 +5,7 @@ package com.sulikdan.ocrApi.controllers.ErrorHandlers;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.sulikdan.ocrApi.errors.UnsupportedLanguageException;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.ProblemDetail;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
@@ -20,14 +20,14 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 public class DocumentUploadExceptionAdvice extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(JsonProcessingException.class)
-    public ResponseEntity<String> handleTesseractException(Exception e) {
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-            .body(JsonProcessingException.class.getName() + " exception:\n" + e.getMessage());
+    public ProblemDetail handleTesseractException(Exception e) {
+        return ProblemDetail.forStatusAndDetail(HttpStatus.INTERNAL_SERVER_ERROR,
+                JsonProcessingException.class.getName() + " exception:\n" + e.getMessage());
     }
 
     @ExceptionHandler(UnsupportedLanguageException.class)
-    public ResponseEntity<String> handleUnsupportedLanguage(Exception e) {
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-            .body(UnsupportedLanguageException.class.getName() + " exception:\n" + e.getMessage());
+    public ProblemDetail handleUnsupportedLanguage(Exception e) {
+        return ProblemDetail.forStatusAndDetail(HttpStatus.INTERNAL_SERVER_ERROR,
+                UnsupportedLanguageException.class.getName() + " exception:\n" + e.getMessage());
     }
 }
