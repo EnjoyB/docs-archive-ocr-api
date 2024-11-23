042d5036ec00bb4efbd3d1f9049122ebb641be66
sulikdan <sulikdan@outlook.com>
2024-11-23 13:28:35 +0100

    basics created

--- frontend/ocr-api-fe/src/app/app.component.ts
+++ frontend/ocr-api-fe/src/app/app.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {HeaderComponent} from "./header/header.component";
+import {BodyComponent} from "./body/body.component";
+import {FooterComponent} from "./footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, BodyComponent, FooterComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  standalone: true
 })
 export class AppComponent {
   title = 'ocr-api-fe';
---
+++ frontend/ocr-api-fe/src/app/body/body.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-body',
+  imports: [],
+  templateUrl: './body.component.html',
+  styleUrl: './body.component.scss',
+  standalone: true
+})
+export class BodyComponent {
+
+}
