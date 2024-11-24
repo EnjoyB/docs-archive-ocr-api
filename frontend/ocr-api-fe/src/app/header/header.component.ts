b8437a8e3bc5582e8ec27a4cb0a5ecc54ade0f3c
sulikdan <sulikdan@outlook.com>
2024-11-24 16:37:22 +0100

    header should be somewhat done

--- frontend/ocr-api-fe/src/app/header/header.component.ts
+++ frontend/ocr-api-fe/src/app/header/header.component.ts
@@ -1,8 +1,19 @@
 import { Component } from '@angular/core';
+import {MatToolbar} from '@angular/material/toolbar';
+import {MatIcon} from '@angular/material/icon';
+import {MatIconButton} from '@angular/material/button';
+import {MatMenu, MatMenuItem, MatMenuTrigger} from '@angular/material/menu';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [
+    MatToolbar,
+    MatIcon,
+    MatIconButton,
+    MatMenu,
+    MatMenuItem,
+    MatMenuTrigger
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
   standalone: true
